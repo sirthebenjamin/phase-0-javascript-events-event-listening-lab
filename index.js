44a949b732a6fa667e22a0adfcb1623b384284c4
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-03-26 23:00:56 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    // Example 1: using an anonymous function as the event handler
+    input.addEventListener('click', function() {
+      alert('I was clicked (Example 1)!');
+    });
+  
+    // Example 2: using a named function as the event handler
+    function clickAlert() {
+      alert('I was clicked (Example 2)!');
+    }
+    input.addEventListener('click', clickAlert);
+  }
